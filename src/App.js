7b1b00d5ac2a271890b72ea23d2129838f3697b6
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-05 22:04:08 -0500

    Added functionality and css for sidenav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React from "react";
 import logo from "./logo.svg";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
+import SideNav from "./sidenav/side-navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { isCollapsed: true };
+  }
+
+  toggleSideNav = () => {
+    this.setState((state) => ({ isCollapsed: !state.isCollapsed }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleSideNav}>
+          <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
+        </button>
+        <SideNav isCollapsed={this.state.isCollapsed}></SideNav>
+        <div
+          className="body"
+          style={{ marginLeft: !this.state.isCollapsed ? "250px" : "0" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>I'm a spinning Chiho</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/sidenav/side-navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./side-navigation.css";
+
+class SideNav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div
+        className="sidenav"
+        style={{ width: !this.props.isCollapsed ? "250px" : "0" }}
+      >
+        <p style={{ color: "white" }}>I'm a side Chiho</p>
+      </div>
+    );
+  }
+}
+export default SideNav;
