f26ce00486101c96902391d9a36195e9170137d2
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-06 02:02:29 -0500

    Setup node backend

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+app.get("/", (req, res) => {
+  res.send("The sedulous hyena ate the antelope!");
+});
+app.listen(port);
