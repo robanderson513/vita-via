a2cb3c4c0c1648a743819be5ccae2e98b08f53f5
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-06 00:19:38 -0500

    Added routing into App

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from "react";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import logo from "./logo.svg";
-import "./App.css";
-import SideNav from "./side-nav/side-nav";
+import { Route } from "react-router-dom";
+import HomePage from "./home-page/home-page";
+import Diet from "./diet/diet";
+import Exercise from "./exercise/exercise";
 import LowerNav from "./lower-nav/lower-nav";
+import "./App.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { isCollapsed: true };
-  }
-
-  toggleSideNav = () => {
-    this.setState((state) => ({ isCollapsed: !state.isCollapsed }));
-  };
-
   render() {
     return (
-      <div className="App">
-        <button className="nav-button" onClick={this.toggleSideNav}>
-          <FontAwesomeIcon className="nav-icon" icon={faBars} />
-        </button>
-        <SideNav isCollapsed={this.state.isCollapsed}></SideNav>
-        <div className="body">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>I'm a spinning Chiho</p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-          <LowerNav></LowerNav>
+      <div>
+        <div>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/diet" exact component={Diet} />
+          <Route path="/exercise" exact component={Exercise} />
         </div>
+        <LowerNav></LowerNav>
       </div>
     );
   }
