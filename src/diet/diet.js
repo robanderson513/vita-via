17f53b087d77bbf1fe00cdfcde27e46ab88fa49d
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-06 00:33:51 -0500

    Created base pages for other tabs

---
+++ src/diet/diet.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import "./lower-nav.css";
+class Diet extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p>Eat Food</p>
+        </div>
+      </div>
+    );
+  }
+}
+export default Diet;
---
+++ src/exercise/exercise.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import "./lower-nav.css";
+class Exercise extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p>Lift Weights</p>
+        </div>
+      </div>
+    );
+  }
+}
+export default Exercise;
