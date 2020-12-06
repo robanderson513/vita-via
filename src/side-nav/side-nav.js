f3a806e232575febfc4aa15bfbc8aa79a595a153
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-06 00:32:30 -0500

    Consolidated logic to side nav

--- src/side-nav/side-nav.js
+++ src/side-nav/side-nav.js
@@ -1,17 +1,29 @@
 import React from "react";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./side-nav.css";
 
 class SideNav extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = { isCollapsed: true };
   }
+
+  toggleSideNav = () => {
+    this.setState((state) => ({ isCollapsed: !state.isCollapsed }));
+  };
   render() {
     return (
-      <div
-        className="side-nav"
-        style={{ width: !this.props.isCollapsed ? "45vw" : "0" }}
-      >
-        <p style={{ color: "white" }}>I'm a side Chiho</p>
+      <div>
+        <button className="nav-button" onClick={this.toggleSideNav}>
+          <FontAwesomeIcon className="nav-icon" icon={faBars} />
+        </button>
+        <div
+          className="side-nav"
+          style={{ width: !this.state.isCollapsed ? "45vw" : "0" }}
+        >
+          <p style={{ color: "white" }}>I'm a side Chiho</p>
+        </div>
       </div>
     );
   }
