00635a7e3417c5cb0919ffa7b8318ec2045042b6
Rob Anderson 🏂 <randerson@assurecare.com>
2020-12-06 23:49:10 -0500

    JS update for ts change

---
+++ backend/dist/models/collection.enum.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Collection = void 0;
+var Collection;
+(function (Collection) {
+    Collection["user"] = "user";
+})(Collection = exports.Collection || (exports.Collection = {}));
+//# sourceMappingURL=collection.enum.js.map
--- backend/dist/models/user.model.js
+++ backend/dist/models/user.model.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    constructor(name) {
-        this.Name = name;
-        this.Phrase = name === "Rob" ? "I'm Rob" : "I'm Chiho";
-    }
-    sayName() {
-        console.log(this.Name);
-    }
-}
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    _id: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: String },
+    number: { type: String },
+    email: { type: String },
+    address: { type: String },
+});
+const User = mongoose_1.model("User", UserSchema);
 exports.User = User;
 //# sourceMappingURL=user.model.js.map
