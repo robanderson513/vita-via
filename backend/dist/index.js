5e178fe959413e8b23943d6fea33ca05e737ed8f
Rob Anderson 🏂 <randerson@assurecare.com>
2022-03-12 23:17:17 -0500

    Updated lock file

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,9 +7,9 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const user_routes_1 = require("./routes/user.routes");
 const PORT = 8000;
-const app = express_1.default();
-app.use(cors_1.default());
-app.get("/", (req, res) => res.send("No"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.get("/", (req, res) => res.send("API HomePage"));
 app.listen(PORT, () => console.log("Server started"));
 //*List of Routes
 app.use("/user", user_routes_1.userRoutes);
